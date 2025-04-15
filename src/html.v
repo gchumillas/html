fe2447b2ee7e730c44ced875a5232b426b0b3e17
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-04-15 09:28:57 +0100

    refactor: rename HTMLElement to Element for consistency

--- src/html.v
+++ src/html.v
@@ -4,28 +4,28 @@ import js.dom
 
 const document = dom.document
 
-pub struct HTMLElement {
+pub struct Element {
 mut:
 	native_element JS.HTMLElement
 }
 
-pub type Component = fn () &HTMLElement
+pub type Component = fn () &Element
 
-pub fn get_element_by_id(id string) ?&HTMLElement {
+pub fn get_element_by_id(id string) ?&Element {
 	native_element := document.getElementById(id.str)?
 
-	return &HTMLElement{
+	return &Element{
 		native_element: native_element
 	}
 }
 
-pub fn create_element(tag_name string) &HTMLElement {
-	return &HTMLElement{
+pub fn create_element(tag_name string) &Element {
+	return &Element{
 		native_element: document.createElement(tag_name.str)
 	}
 }
 
-pub fn (target &HTMLElement) add_event_listener(event string, cb fn ()) {
+pub fn (target &Element) add_event_listener(event string, cb fn ()) {
 	target.native_element.addEventListener(
 		event.str,
 		fn [cb] (_ JS.Event) { cb() },
@@ -33,18 +33,18 @@ pub fn (target &HTMLElement) add_event_listener(event string, cb fn ()) {
 	)
 }
 
-pub fn (target &HTMLElement) render(comp Component) {
+pub fn (target &Element) render(comp Component) {
 	target.native_element.appendChild(comp().native_element)
 }
 
-pub fn (mut target HTMLElement) append_child(el &HTMLElement) {
+pub fn (mut target Element) append_child(el &Element) {
 	target.native_element.appendChild(el.native_element)
 }
 
-pub fn (target &HTMLElement) get_inner_text() string {
+pub fn (target &Element) get_inner_text() string {
 	return string(target.native_element.innerText)
 }
 
-pub fn (mut target HTMLElement) set_inner_text(text string) {
+pub fn (mut target Element) set_inner_text(text string) {
 	target.native_element.innerText = text.str
 }
